3ccefc5fdf22ceffe4d7b6c2d4472aca7209cff4
kasidej02 <kasidej.kammool@gmail.com>
2020-01-21 15:33:54 +0700

    number 3

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,1 +1,44 @@
 //status = "ONE-NIGHT-STAND";
+#include<iostream>
+#include<string>
+using namespace std;
+
+
+int main(){
+    string status ="";
+    int age,h,m;
+    cout<<"Age : ";
+    cin>>age;
+    if(age<=20){
+        cout<<"Height : ";
+        cin>>h;
+        if(h<=160){
+            status = "UNFRIEND";
+            
+        }else if(h<175){
+            status = "FRIEND";
+            
+        }else{
+            cout<<"Money : ";
+            cin>>m;
+            if(m>69000000){
+            status = "MARRIED";
+        }else{
+            status = "ONE NIGHT STAND";
+        }
+        } 
+    }else if(age<30){
+        cout<<"Money  : " ;
+        cin>>m;
+        if(m>10000000){
+            status = "BEST FRIEND";
+        }else{
+            status = "UNFRIEND";
+        }
+    }else
+    {
+        status = "UNFREIND";
+    }
+    
+    cout<< status ;
+}
